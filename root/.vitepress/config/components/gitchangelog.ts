5376ac85f9d82076de6d474045aef50c07a8e341
Glomzzz <glom@skillw.com>
2024-05-20 04:36:02 +0800

    (fix) git-changelog-locales

--- root/.vitepress/config/components/gitchangelog.ts
+++ root/.vitepress/config/components/gitchangelog.ts
@@ -1,21 +1,35 @@
 import { localesOf } from "../locales";
 
-import { Locale } from '@nolebase/vitepress-plugin-git-changelog/client'
+import { Locale } from "@nolebase/vitepress-plugin-git-changelog/client";
 
-export const locales = localesOf<Locale>({ 
-      'en': {
-        noLogs: 'No recent changes',
-        lastEdited: 'This page was last edited {{daysAgo}}',
-        lastEditedDateFnsLocaleName: 'enUS',
-        viewFullHistory: 'View full history',
-        committedOn: ' on {{date}}', 
-      },
-      'zh-CN': {
-        noLogs: '暂无最近变更历史',
-        lastEdited: '本页面最后编辑于 {{daysAgo}}',
-        lastEditedDateFnsLocaleName: 'zhCN',
-        viewFullHistory: '查看完整历史',
-        committedOn: '于 {{date}} 提交',
-       },
-     }
-)
+export const locales = localesOf<Locale>({
+  en: {
+    changelog: {
+      title: "Changelog",
+      noData: "No recent changes",
+      lastEdited: "Last edited {{daysAgo}}",
+      lastEditedDateFnsLocaleName: "enUS",
+      viewFullHistory: "View full history",
+      committedOn: " on {{date}}",
+    },
+    contributors: {
+      title: "Contributors",
+      noData: "No contributors",
+    },
+  },
+  'zh-CN': {
+    changelog: {
+      title: "页面历史",
+      noData: "暂无最近变更历史",
+      lastEdited: "最后编辑于 {{daysAgo}}",
+      lastEditedDateFnsLocaleName: "zhCN",
+      viewFullHistory: "查看完整历史",
+      committedOn: " 于 {{date}}",
+    },
+
+    contributors: {
+      title: "贡献者",
+      noData: "暂无相关贡献者",
+    },
+  },
+});
